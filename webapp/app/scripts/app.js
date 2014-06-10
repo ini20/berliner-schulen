0ab343b95da250b14c548ef36991f75b8a7ec24e
Markus Holtermann <info@markusholtermann.eu>
2014-06-10 18:02:48 +0200

    Fixes #18 -- Integrate elasticsearch JavaScript API

--- webapp/app/scripts/app.js
+++ webapp/app/scripts/app.js
@@ -1,28 +1,40 @@
 'use strict';
 
-angular
-  .module('schooldataApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'localytics.directives'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/schools/:schoolId', {
-        templateUrl: 'views/school.html',
-        controller: 'SchoolCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+(function(){
+    var app = angular
+    .module('schooldataApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'localytics.directives',
+        'elasticsearch'
+    ]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+            })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'AboutCtrl'
+        })
+        .when('/schools/:schoolId', {
+            templateUrl: 'views/school.html',
+            controller: 'SchoolCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    });
+
+    app.service('es', function (esFactory) {
+        console.log('es', esFactory);
+        return esFactory({
+            host: sp.config.elasticsearch.host
+        });
+    });
+    return app;
+})();
--- webapp/app/scripts/controllers/main.js
+++ webapp/app/scripts/controllers/main.js
@@ -1,55 +1,58 @@
 'use strict';
 
 angular.module('schooldataApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    var es = sp.config.elasticsearch;
-    var query = {
-      aggs: {
-        district: {
-          terms: {
-            field: 'name',
-            size: 0
-          }
-        }
-      }
-    };
-    var url = es.host + '/' + es.index + '/district/_search';
+    .controller('MainCtrl', ['$scope', '$http', 'es', function ($scope, $http, es) {
+        console.log(es);
+        es.search({
+            index: sp.config.elasticsearch.index,
+            type: 'district',
+            body: {
+                aggs: {
+                    district: {
+                        terms: {
+                            field: 'name',
+                            size: 0
+                        }
+                    }
+                }
+            }
+        }).then(function (body) {
+            var districts = [];
+            $.each(body.aggregations.district.buckets, function(i, v){
+                districts.push(v.key);
+            });
+            $scope.districts = districts;
+        }, function (error) {
+            console.trace(error.message);
+        });
 
-    $http.post(url, query).success(function(data) {
-      var districts = [];
-      $.each(data.aggregations.district.buckets, function(i, v){
-        districts.push(v.key);
-      });
-      $scope.districts = districts;
-    });
-
-    $scope.schoolTypes = [
-      'Schulpraktisches Seminar',
-      'Berlin-Kolleg',
-      'Berufsschule',
-      'Berufsfachschule',
-      'Fachoberschule',
-      'Fachschule',
-      'Berufliches Gymnasium',
-      'Berufsoberschule',
-      'Abend-Gymnasium',
-      'Abend-Hauptschule',
-      'Abend-Realschule',
-      'Ausländische Schule',
-      'Freie Waldorfschule',
-      'Grundschule',
-      'Gymnasium',
-      'Heilpraktikerschule',
-      'Integrierte Sekundarschule',
-      'Kosmetikschule',
-      'Künstlerische Schule',
-      'Sonstige Ergänzungsschule',
-      'Sprachschulen',
-      'Volkshochschul-Kolleg',
-      'Volkshochschule',
-      'Vorbereitungsschule auf Fremdenprüfung',
-      'Wirtschafts- u. Verwaltungsschule'
-    ];
-    $scope.languages = ['Deutsch', 'Englisch', 'Französisch'];
-    $scope.test = ['l'];
-  });
+        $scope.schoolTypes = [
+            'Schulpraktisches Seminar',
+            'Berlin-Kolleg',
+            'Berufsschule',
+            'Berufsfachschule',
+            'Fachoberschule',
+            'Fachschule',
+            'Berufliches Gymnasium',
+            'Berufsoberschule',
+            'Abend-Gymnasium',
+            'Abend-Hauptschule',
+            'Abend-Realschule',
+            'Ausländische Schule',
+            'Freie Waldorfschule',
+            'Grundschule',
+            'Gymnasium',
+            'Heilpraktikerschule',
+            'Integrierte Sekundarschule',
+            'Kosmetikschule',
+            'Künstlerische Schule',
+            'Sonstige Ergänzungsschule',
+            'Sprachschulen',
+            'Volkshochschul-Kolleg',
+            'Volkshochschule',
+            'Vorbereitungsschule auf Fremdenprüfung',
+            'Wirtschafts- u. Verwaltungsschule'
+        ];
+        $scope.languages = ['Deutsch', 'Englisch', 'Französisch'];
+        $scope.test = ['l'];
+    }]);
