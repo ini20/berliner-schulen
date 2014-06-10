3883e3a6a36a693cf3844f8757e830d8be3c5128
Markus Holtermann <info@markusholtermann.eu>
2014-06-10 18:04:05 +0200

    Reindent

--- webapp/app/scripts/controllers/about.js
+++ webapp/app/scripts/controllers/about.js
@@ -8,10 +8,10 @@
  * Controller of the schooldataApp
  */
 angular.module('schooldataApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('AboutCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
--- webapp/app/scripts/controllers/map.js
+++ webapp/app/scripts/controllers/map.js
@@ -1,18 +1,18 @@
 'use strict';
 
 angular.module('schooldataApp')
-  .controller('MapCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.map=new OpenLayers.Map('map');
-    $scope.map.addControl(new OpenLayers.Control.LayerSwitcher());
-    var a=new OpenLayers.Layer.OSM.Mapnik('Maplint');
-    $scope.map.addLayer(a);
-    $scope.map.zoomToMaxExtent();
-    var markers=new OpenLayers.Layer.Markers('Markers');
-    $scope.map.addLayer(markers);
-    var b=new OpenLayers.LonLat(13.408056,52.518611).transform(new OpenLayers.Projection('EPSG:4326'), $scope.map.getProjectionObject());
-    $scope.map.setCenter(b,12);
-    $scope.loadMarkers = function() {
-      $http.get().success(function(data) {
-      });
-    };
-  }]);
+    .controller('MapCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.map=new OpenLayers.Map('map');
+        $scope.map.addControl(new OpenLayers.Control.LayerSwitcher());
+        var a=new OpenLayers.Layer.OSM.Mapnik('Maplint');
+        $scope.map.addLayer(a);
+        $scope.map.zoomToMaxExtent();
+        var markers=new OpenLayers.Layer.Markers('Markers');
+        $scope.map.addLayer(markers);
+        var b=new OpenLayers.LonLat(13.408056,52.518611).transform(new OpenLayers.Projection('EPSG:4326'), $scope.map.getProjectionObject());
+        $scope.map.setCenter(b,12);
+        $scope.loadMarkers = function() {
+            $http.get().success(function(data) {
+            });
+        };
+    }]);
