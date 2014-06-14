56270db9006449b0eecfb8edee83a019674ae00f
Alexander.Grießer <gieser@bietigheimer-htc.de>
2014-06-14 20:43:40 +0200

    Added a navigation in the top bar. Highlighting of the currently "visited page"

--- webapp/app/scripts/app.js
+++ webapp/app/scripts/app.js
@@ -92,5 +92,27 @@ var sp = sp || {};
 
         return mapService;
     }]);
+
+    mod.directive('activeLink', ['$location', function($location) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs, controller) {
+                var clazz = "active";
+                if (attrs.activeLink != "") {
+                    clazz = attrs.activeLink;
+                }
+                var path = attrs.href;
+                path = path.substring(1); //hack because path does not return including hashbang
+                scope.location = $location;
+                scope.$watch('location.path()', function(newPath) {
+                    if (path === newPath) {
+                        element.parent().addClass(clazz);
+                    } else {
+                        element.parent().removeClass(clazz);
+                    }
+                });
+            } 
+        };
+    }]);
     return mod;
 })();
