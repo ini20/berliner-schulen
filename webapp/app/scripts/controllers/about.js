25b4fe842347690efa88c66cff8940ffc6ac2a8a
Markus Holtermann <info@markusholtermann.eu>
2014-06-17 16:28:36 +0200

    Fixes DI

--- webapp/app/scripts/controllers/about.js
+++ webapp/app/scripts/controllers/about.js
@@ -8,10 +8,10 @@
  * Controller of the schooldataApp
  */
 angular.module('schooldataApp')
-    .controller('AboutCtrl', function ($scope) {
+    .controller('AboutCtrl', ['$scope', function ($scope) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
-    });
+    }]);
