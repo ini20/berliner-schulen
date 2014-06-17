fc3b47adc03cee0e8615c9bd55151c03c034ec8d
Markus Holtermann <info@markusholtermann.eu>
2014-06-17 16:28:54 +0200

    Fix indention

--- webapp/app/scripts/controllers/school.js
+++ webapp/app/scripts/controllers/school.js
@@ -17,37 +17,37 @@ angular.module('schooldataApp')
 
     var body = {};
     body.query = {
-    	bool: {
-    		must : [{
-	    	query_string: {
-	            default_field: 'school.bsn',
-	            query: $routeParams.schoolId
-	        }}]
+        bool: {
+            must : [{
+            query_string: {
+                default_field: 'school.bsn',
+                query: $routeParams.schoolId
+            }}]
     }};
 
     body.size = 1;
 
     this.loadSchool = function() {
-    	$scope.loading = 1;
-    	$scope.error = null;
-	    es.search({
-		        index: sp.config.elasticsearch.index,
-		        type: 'school',
-		        body: body
-		    }).then(function (returned) {
-		    	$scope.schoolData = {}; // reset from possible previous value
-		    	// we found the school
-		    	if (returned.hits.total > 0) {
-		    		$scope.schoolData = returned.hits.hits[0]._source;
-		    	} else {
-		    		$scope.error = "";
-		    	}
-		    	$scope.loading = null;
-		    }, function(error) {
-		    	$scope.error = "";
-		    	$scope.loading = null;
-		    	console.log(error.message);
-	    });
+        $scope.loading = 1;
+        $scope.error = null;
+        es.search({
+                index: sp.config.elasticsearch.index,
+                type: 'school',
+                body: body
+            }).then(function (returned) {
+                $scope.schoolData = {}; // reset from possible previous value
+                // we found the school
+                if (returned.hits.total > 0) {
+                    $scope.schoolData = returned.hits.hits[0]._source;
+                } else {
+                    $scope.error = "";
+                }
+                $scope.loading = null;
+            }, function(error) {
+                $scope.error = "";
+                $scope.loading = null;
+                console.log(error.message);
+        });
     };
 
     this.loadSchool();
