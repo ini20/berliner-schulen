d92128d886c5e228d42de8726e7d3f5090d7a9fd
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-02-21 18:06:21 +0100

    add .sass-cache dir in clean task

--- webapp/Gruntfile.js
+++ webapp/Gruntfile.js
@@ -123,6 +123,7 @@ module.exports = function (grunt) {
           dot: true,
           src: [
             '.tmp',
+            '.sass-cache',
             '<%= yeoman.dist %>/{,*/}*',
             '!<%= yeoman.dist %>/.git*'
           ]
