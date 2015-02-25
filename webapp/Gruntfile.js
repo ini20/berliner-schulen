55f6072ff2078870e13d771821484f0e61ea7608
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-02-25 10:40:15 +0100

    grunt watch watches new all js files not only in  dir

--- webapp/Gruntfile.js
+++ webapp/Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function (grunt) {
         tasks: ['wiredep']
       },
       js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        files: ['<%= yeoman.app %>/{,*/}*.js'],
         tasks: ['newer:jshint:all'],
         options: {
           livereload: '<%= connect.options.livereload %>'
