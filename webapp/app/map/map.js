f89d30a5472088cc325c4844a877c2b1c562ec5c
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-03-24 11:59:12 +0100

    update map module

--- webapp/app/map/map.js
+++ webapp/app/map/map.js
@@ -2,6 +2,14 @@
 
 angular.module('berlinerSchulenApp')
 	.controller('MapCtrl', ['$scope', 'schoolFactory', function($scope, schoolFactory){
+
+		/* This is our Map setup.
+		 *
+		 * It uses the OSM tiles and is set to zoom 14.
+		 * The map is centered to Berlin and zooming with the mouse
+		 * is disabled. It is disabled because it made navigation on the
+		 * page difficult.
+		 */
 		angular.extend($scope, {
 			defaults: {
 				tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
@@ -18,21 +26,30 @@ angular.module('berlinerSchulenApp')
 				lng: 13.3833154,
 				zoom: 10
 			},
-			data: {markers: {}}
-		});
-
-		$scope.data.markers = {};
-		angular.extend($scope.data, {
-			markers: {
-				m1: {
-					lat: 52.5153601,
-					lng: 13.3833154,
-					compileMessage: false,
-					message: 'I\'m a static marker',
+			data: {
+				markers: {
+					// The following is a sample marker and is only shown if
+					// the JSON file with all schools cannot be fetched and
+					// therefore no data is available to be shown.
+					m1: {
+						lat: 52.5153601,
+						lng: 13.3833154,
+						compileMessage: false,
+						message: 'Das ist Berlin. Für den Fall, dass<br>du das noch nicht wusstest :)',
+					}
 				}
 			}
 		});
 
+		// Tell the schoolFactory to get the JSON file which contains
+		// all the data.
+		schoolFactory.getJson();
+
+		/* This $on method is called if the factory has a new dataset
+		 * available and broadcasts it to all controllers which subscribed
+		 * to this name `updateSchools`.
+		 * -> This is kind of an Event-Driven-Design
+		 */
 		$scope.$on('updateSchools', function() {
 
 			/* Simple function to check if a value is a float */
@@ -65,9 +82,9 @@ angular.module('berlinerSchulenApp')
 
 				}
 
-				if ( schools.length - i > 100) {
-					break;
-				}
+				// if ( schools.length - i > 100) {
+				// 	break;
+				// }
 			}
 
 			// the markers object, which is used by the map does not want
@@ -89,94 +106,3 @@ angular.module('berlinerSchulenApp')
 		});
 
 }]);
-
-/** *********** */
-/** OLD VERSION */
-/** *********** */
-
-// angular.module('berlinerSchulenApp')
-//     .controller('MapCtrl', ['$scope', '$http', '$interpolate', '$timeout', 'mapService', function ($scope, $http, $interpolate, $timeout, mapService) {
-//         var center = new OpenLayers.Geometry.Point(13.383333, 52.516667)
-//             .transform('EPSG:4326', 'EPSG:3857');
-
-//         var mapbox = new OpenLayers.Layer.XYZ("Berliner Schulen Karte",
-//             ["http://a.tiles.mapbox.com/v4/obstschale.kp8hf045/${z}/${x}/${y}.png?access_token=pk.eyJ1Ijoib2JzdHNjaGFsZSIsImEiOiJvSFdVbmRRIn0.2aQ9TaMbMbyrAuFQh_icXg"], {
-//             sphericalMercator: true,
-//         });
-
-//         $scope.map = new OpenLayers.Map('map', {
-//             projection: 'EPSG:3857',
-//             layers: [mapbox],
-//             center: center.getBounds().getCenterLonLat(),
-//             zoom: 10,
-//         });
-
-//         var popupTemplate;
-//         $http({
-//                 method: 'GET',
-//                 url: sp.config.map.feature_bubble,
-//                 cache: true
-//             }).
-//             success(function(data) {
-//                 popupTemplate = $interpolate(data);
-//             }).
-//             error(function() {
-//                 popupTemplate = $interpolate('<div data-alert class="alert-box alert">Template not found!</div>');
-//         });
-
-//         var overlay = new OpenLayers.Layer.Vector('Overlay', {
-//             eventListeners: {
-//                 featureselected: function(evt){
-//                     var feature = evt.feature;
-//                     var popup = new OpenLayers.Popup.FramedCloud('popup',
-//                         OpenLayers.LonLat.fromString(feature.geometry.toShortString()),
-//                         null,
-//                         popupTemplate(feature.data),
-//                         null,
-//                         true,
-//                         null
-//                     );
-//                     popup.autoSize = true;
-//                     popup.maxSize = new OpenLayers.Size(400,800);
-//                     popup.fixedRelativePosition = true;
-//                     feature.popup = popup;
-//                     $scope.map.addPopup(popup);
-//                 },
-//                 featureunselected: function(evt){
-//                     var feature = evt.feature;
-//                     $scope.map.removePopup(feature.popup);
-//                     feature.popup.destroy();
-//                     feature.popup = null;
-//                 }
-//             }
-//         });
-
-//         var selector = new OpenLayers.Control.SelectFeature(overlay, {
-//             autoActivate: true,
-//             hover: false,
-//             toggle: true,
-//         });
-//         $scope.map.addControl(new OpenLayers.Control.LayerSwitcher());
-//         $scope.map.addControl(selector);
-//         // $scope.map.addLayer(mapnik);
-//         $scope.map.addLayer(overlay);
-
-//         $scope.display_done = true;
-
-//         $scope.$on('updateMapMarkers', function() {
-//             overlay.removeAllFeatures();
-//             if (mapService.markers.length > 0) {
-//                 $scope.display_done = false;
-//                 var cnt = 0.0;
-//                 angular.forEach(mapService.markers, function(marker) {
-//                     $timeout(function(){
-//                         overlay.addFeatures([marker]);
-//                         cnt += 1.0;
-//                         if (cnt >= mapService.markers.length) {
-//                             $scope.display_done = true;
-//                         }
-//                     }, 1);
-//                 });
-//             }
-//         });
-//     }]);
