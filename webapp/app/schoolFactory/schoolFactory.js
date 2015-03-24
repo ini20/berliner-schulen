eac51e3ff8421a2632230a2e85452d72ae9bd3fb
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-03-24 12:00:18 +0100

    Add setFilter and applyFilter function in schoolFactory

--- webapp/app/schoolFactory/schoolFactory.js
+++ webapp/app/schoolFactory/schoolFactory.js
@@ -2,13 +2,28 @@
 
 angular.module('berlinerSchulenApp')
 	.factory('schoolFactory', ['$http', '$rootScope', function($http, $rootScope){
-		var schools = {content:null};
 
-		schools.updateFilter = function(val) {
-			schools.content = [val];
-			if (val === 'new' || val === '') {
-				schools.getJson();
-			}
+		var schools = {content:null};
+		var filter = {};
+
+		/**
+		 * set new filter properties but do not apply filter
+		 * @param Obj filerProp new filter properties
+		 * @return Obj this
+		 */
+		schools.setFilter = function(filterProp) {
+			filter = filterProp;
+			return schools;
+		};
+
+		/**
+		 * start filtering and broadcast new school set
+		 * @return Obj this
+		 */
+		schools.applyFilter = function() {
+			console.log('filter: ' + filter);
+
+			return schools;
 		};
 
 		schools.getJson = function() {
