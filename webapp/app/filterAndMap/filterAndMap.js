50b59d18382a8b3ca6cac687af0e4e9147b67bb0
Janekdererste <janek.laudan@mailbox.tu-berlin.de>
2015-03-31 16:31:41 +0200

    feature/#40
    * filter und Map in eineLayout Datei gelegt.
    * unnötiges Padding von zusatzfiltern entfernt.

---
+++ webapp/app/filterAndMap/filterAndMap.js
@@ -0,0 +1,5 @@
+angular.module('berlinerSchulenApp')
+	.controller('FilterAndMapCtrl', ['$scope', '$timeout', '$mdSidenav', 'schoolFactory',
+		function ($scope, $timeout, $mdSidenav, schoolFactory) {
+			$scope.test = "hallo";
+		}]);
